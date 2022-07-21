09c8b9567b17de3d5150df31dcdbb24c6307c055
harujamu <tnagisa1220@gmail.com>
2022-07-21 05:28:44 +0000

    [Save]Homesコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip:[:registrations, :passwords], controllers:{
+  
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about", as: "about"
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+ 
   
-  devise_for :customers, skip:[:passwords], controllers:{
+  devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
