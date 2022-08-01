7df6008511a19400bcec2c7d3a4b269603f8ea1b
harujamu <tnagisa1220@gmail.com>
2022-08-01 13:50:03 +0000

    [Save]layout/links_shared修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def configure_permitted_parameters
-    p "Hello!!!!!!!"
     devise_parameter_sanitizer.permit(:sign_up,keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :telephone_number, :address])
   end  
 end
--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,27 @@
 class Public:: AddressesController < ApplicationController
   
   def index
+    addresses = Address.all
+    @ones_addresses = @customer.addresses
   end
 
   def edit
   end
 
   def create
+    @address = Address.new(address_params)
   end
 
   def update
   end
   
   def destroy
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:name,:postal_code,:address)
   end
   
 end
