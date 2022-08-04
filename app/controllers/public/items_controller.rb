8c5c209ce964f6b85dbdebd3ebf94c53db6a9107
harujamu <tnagisa1220@gmail.com>
2022-08-04 05:21:55 +0000

    [Save]顧客用画面（商品一覧編集）

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,9 +2,16 @@ class Public::ItemsController < ApplicationController
   
   def index
     @genres = Genre.all
+    @items = Item.page(params[:page])
   end
   
   def show
   end
+  
+  private
+  
+    def item_params
+      params.require(:item).permit(:name,:introduction,:price,:is_active, :genre_id, :image)
+    end
   
 end
