588e8bbcb9d095132b72fc2335d201c346027713
harujamu <tnagisa1220@gmail.com>
2022-08-20 13:28:30 +0000

    [Save]顧客用画面（OrderController編集）

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -35,15 +35,21 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(order_params)
+    @order = current_customer.orders.build(order_params)
     @order.save
-    
+
+    # each文の始まり
+    current_customer.cart_items.each do |cart_item|
     @order_detail = OrderDetail.new
     @order_detail.order_id = @order.id
-    @order_detail.production_status = OrderDetail.production_statuses.key(0)
-    
-    @order_detail.save
-   
+    # @order_detail.production_status = OrderDetail.production_statuses.key(0)
+    @order_detail.amount = cart_item.amount
+    @order_detail.tax_included_price = cart_item.item.price * 1.1
+    @order_detail.item_id = cart_item.item_id
+    byebug
+    @order_detail.save!
+    end
+    current_customer.cart_items.destroy_all
     redirect_to orders_complete_path
   end
 
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   validates:last_name, presence:true
   validates:first_name, presence:true
   validates:last_name_kana, presence:true
@@ -14,8 +14,8 @@ class Customer < ApplicationRecord
   validates:address, presence:true
   validates:telephone_number, presence:true
   validates:is_deleted,inclusion:[true, false]
-  
+
   has_many :addresses, dependent: :destroy
   has_many :cart_items, dependent: :destroy
-  has_many :order
+  has_many :orders
 end
